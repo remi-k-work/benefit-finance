8b3aa8df1a4740631c47732a27bdeac0c07bbab9
Remi <remi.k.work@gmail.com>
2026-01-21 13:44:56 +0100

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
+  typedRoutes: true,
+
+  experimental: {
+    authInterrupts: true,
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "c5a4npv9he.ufs.sh",
+        pathname: "/f/*",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
